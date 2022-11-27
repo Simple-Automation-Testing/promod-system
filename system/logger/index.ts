217f897378a592bb71097f6fa1f0b3e14ae44717
Potapov Dmitriy <potapovd3012@gmail.com>
2022-11-27 08:48:23 +0200

    improve logger

--- system/logger/index.ts
+++ system/logger/index.ts
@@ -1,11 +1,12 @@
 import { createLogger } from 'sat-utils';
 
-const logger = createLogger();
-
-logger.addCustomLevel('promodSystem', 'PROMOD_SYSTEM', 'PROMOD_SYSTEM', 'info', 'BgGreen', 'Underscore');
-
-type TLogger = typeof logger & { promodSystem(...args: any[]): void };
-
-const promodLogger = logger as TLogger;
+const promodLogger = createLogger().addCustomLevel(
+  'promodSystem',
+  'PROMOD_SYSTEM',
+  'PROMOD_SYSTEM',
+  'info',
+  'BgGreen',
+  'Underscore',
+);
 
 export { promodLogger };
