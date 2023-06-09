69e8c814e41ce5f1f65d701783d8171b03a70cd4
Potapov Dmitriy <potapovd3012@gmail.com>
2023-06-09 18:53:30 +0300

    fix unknown types

--- system/generator/create.type.ts
+++ system/generator/create.type.ts
@@ -1,9 +1,10 @@
+/* eslint-disable no-console */
 function createType(itemObject: Array<{ [k: string]: { [k: string]: string } }> | { [k: string]: string }, action) {
   if (itemObject[action]) {
     return `${itemObject[action]}`;
   }
 
-  return (itemObject as Array<{ [k: string]: { [k: string]: string } }>)
+  const generatedTypeString = (itemObject as Array<{ [k: string]: { [k: string]: string } }>)
     .filter(item => item[Object.keys(item)[0]][action])
     .reduce((typeString, fieldDescriptor, index, initialArr) => {
       const field = Object.keys(fieldDescriptor)[0];
@@ -15,6 +16,15 @@ function createType(itemObject: Array<{ [k: string]: { [k: string]: string } }> 
               : `\n ${field}?: ${fieldDescriptor[field][action]}`)
         : typeString;
     }, '{');
+
+  if (generatedTypeString === '{') {
+    // TODO need to find a solution to make
+    console.log('Issue, type generation does not handle properly ');
+
+    return 'unknown';
+  }
+
+  return generatedTypeString;
 }
 
 export { createType };
